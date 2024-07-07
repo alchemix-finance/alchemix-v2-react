94e4fb27abc969fd634aa87f9e5809e01c6a69dd
t0rbik <liker1011@gmail.com>
2024-07-07 16:10:34 +0200

    hide router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,22 @@
+import { lazy, Suspense } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { Layout } from "@/components/layout/Layout";
 import { Toaster } from "@/components/ui/sonner";
 
 import "@/styles/index.css";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRoute({
   component: () => {
@@ -14,7 +26,9 @@ export const Route = createRootRoute({
           <Outlet />
         </Layout>
         <Toaster position="top-center" />
-        <TanStackRouterDevtools />
+        <Suspense>
+          <TanStackRouterDevtools />
+        </Suspense>
       </>
     );
   },
