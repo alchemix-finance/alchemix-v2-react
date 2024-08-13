c085658b8d6cdb9574b62e36bf2ee835c8af7251
t0rbik <liker1011@gmail.com>
2024-08-13 11:29:26 +0200

    error page, vaults and transmuters keys to reset state when chain changes

--- src/routes/transmuters.tsx
+++ src/routes/transmuters.tsx
@@ -1,19 +1,23 @@
 import { Page } from "@/components/common/Page";
+import { ErrorComponent } from "@/components/error/ErrorComponent";
 import { Transmuters } from "@/components/transmuters/Transmuters";
+import { useChain } from "@/hooks/useChain";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/transmuters")({
   component: TransmutersRoute,
+  errorComponent: ErrorComponent,
 });
 
 function TransmutersRoute() {
+  const chain = useChain();
   return (
     <Page
       title="Transmuter"
       description="Convert your synthetic tokens to their counterpart at a 1:1 ratio"
       iconUri="/images/icons/transmuter_thin.svg"
     >
-      <Transmuters />
+      <Transmuters key={chain.id} />
     </Page>
   );
 }
