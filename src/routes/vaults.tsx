39b93f189de7d483a57296cacc89d39239f4af92
t0rbik <liker1011@gmail.com>
2024-09-02 20:27:25 +0200

    change alchemix icons

--- src/routes/vaults.tsx
+++ src/routes/vaults.tsx
@@ -15,7 +15,7 @@ function VaultsRoute() {
     <Page
       title="Vaults"
       description="Deposit collateral and borrow your future yield right away."
-      iconUri="/images/icons/yield_thin.svg"
+      iconUri="/images/icons/vaults_thin.svg"
     >
       <Vaults key={chain.id} />
     </Page>
