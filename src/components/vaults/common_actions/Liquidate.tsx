f516be20ea39627287c6fcdc3cb1e9ae50d130fb
t0rbik <liker1011@gmail.com>
2024-10-17 20:27:52 +0200

    update liqudation to follow withdraw shares overflow workaroun

--- src/components/vaults/common_actions/Liquidate.tsx
+++ src/components/vaults/common_actions/Liquidate.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/select";
 import { useTokensQuery } from "@/lib/queries/useTokensQuery";
 import {
+  useAccount,
   useReadContract,
   useSimulateContract,
   useWaitForTransactionReceipt,
@@ -33,11 +34,14 @@ import { MAX_UINT256_BN } from "@/lib/constants";
 import { invalidateWagmiUseQueryPredicate } from "@/utils/helpers/invalidateWagmiUseQueryPredicate";
 import { CtaButton } from "@/components/common/CtaButton";
 import { getTokenLogoUrl } from "@/utils/getTokenLogoUrl";
+import { useVaultsLiquidateAvailableBalance } from "@/lib/queries/vaults/useVaultsLiquidateAvailableBalance";
 
 export const Liquidate = () => {
   const queryClient = useQueryClient();
   const chain = useChain();
   const mutationCallback = useWriteContractMutationCallback();
+
+  const { address } = useAccount();
 
   const [amount, setAmount] = useState("");
   const [slippage, setSlippage] = useState("0.5");
@@ -90,7 +94,7 @@ export const Liquidate = () => {
       v.yieldToken.toLowerCase() === liquidationTokenAddress?.toLowerCase(),
   );
 
-  const { data: shares } = useReadContract({
+  let { data: shares } = useReadContract({
     address: vault?.alchemist.address,
     abi: alchemistV2Abi,
     chainId: chain.id,
@@ -103,6 +107,33 @@ export const Liquidate = () => {
       enabled: !!vault && !isInputZero(amount),
     },
   });
+
+  const { data: depositedSharesBalance } = useReadContract({
+    address: vault?.alchemist.address,
+    abi: alchemistV2Abi,
+    chainId: chain.id,
+    functionName: "positions",
+    args: [address!, vault?.yieldToken ?? zeroAddress],
+    query: {
+      enabled: !!address,
+      select: ([shares]) => shares,
+    },
+  });
+
+  const { balance } = useVaultsLiquidateAvailableBalance({ vault });
+
+  /**
+   * Guard against dynamic yield token price changes.
+   * When initially calculated max yield tokens amount from static shares,
+   * converts back to shares higher than deposited shares balance
+   */
+  if (
+    depositedSharesBalance !== undefined &&
+    shares !== undefined &&
+    depositedSharesBalance < shares
+  ) {
+    shares = depositedSharesBalance - 1n;
+  }
 
   const { data: minimumOut } = useReadContract({
     address: vault?.alchemist.address,
@@ -215,6 +246,13 @@ export const Liquidate = () => {
     }
   };
 
+  const isInsufficientBalance = balance !== undefined && +amount > +balance;
+  const isDisabledCta =
+    isPending ||
+    isInputZero(amount) ||
+    !confirmedLiquidation ||
+    isInsufficientBalance;
+
   return (
     <div className="space-y-4 bg-grey15inverse p-4 dark:bg-grey15">
       <DebtSelection
@@ -283,9 +321,9 @@ export const Liquidate = () => {
             variant="outline"
             width="full"
             onClick={onCtaClick}
-            disabled={isPending || isInputZero(amount) || !confirmedLiquidation}
+            disabled={isDisabledCta}
           >
-            Liquidate
+            {isInsufficientBalance ? "Insufficient Balance" : "Liquidate"}
           </CtaButton>
         </>
       )}
