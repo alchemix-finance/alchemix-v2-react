1ff92d01578b6860db0773e9b7727da3fab876d1
t0rbik <liker1011@gmail.com>
2024-10-29 16:20:20 +0100

    give life to cta buttons

--- src/components/common/CtaButton.tsx
+++ src/components/common/CtaButton.tsx
@@ -1,12 +1,18 @@
 import { useAccount } from "wagmi";
-import { Button } from "../ui/button";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { ComponentPropsWithRef } from "react";
+import { AnimatePresence, m } from "framer-motion";
+
+import { Button } from "@/components/ui/button";
 
 /**
  * Button that opens connect modal if the user is not connected.
  */
-export const CtaButton = (props: ComponentPropsWithRef<typeof Button>) => {
+export const CtaButton = (
+  props: Omit<ComponentPropsWithRef<typeof Button>, "children"> & {
+    children: string;
+  },
+) => {
   const { address } = useAccount();
   const { openConnectModal } = useConnectModal();
   return (
@@ -15,7 +21,17 @@ export const CtaButton = (props: ComponentPropsWithRef<typeof Button>) => {
       onClick={!address ? openConnectModal : props.onClick}
       disabled={!address ? false : props.disabled}
     >
-      {!address ? "Connect Wallet" : props.children}
+      <AnimatePresence initial={false} mode="popLayout">
+        <m.span
+          key={!address ? "connect wallet" : props.children}
+          transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+          initial={{ opacity: 0, y: -25 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 25 }}
+        >
+          {!address ? "Connect Wallet" : props.children}
+        </m.span>
+      </AnimatePresence>
     </Button>
   );
 };
