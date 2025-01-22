c152cac54e2d168d85842cf917f667cb4bf20c01
t0rbik <liker1011@gmail.com>
2025-01-22 22:00:15 +0100

    add updates rives and adjust for mbiles

--- src/components/landing/RiveSection.tsx
+++ src/components/landing/RiveSection.tsx
@@ -2,7 +2,7 @@ import Rive from "@rive-app/react-canvas";
 
 export const RiveSection = () => {
   return (
-    <div className="flex flex-col justify-center gap-4 lg:flex-row">
+    <div className="pointer-events-none flex flex-col justify-center gap-4 lg:pointer-events-auto lg:flex-row">
       <Rive
         className="h-[330px] w-[330px] lg:h-44 lg:w-44 xl:h-72 xl:w-72 2xl:h-[330px] 2xl:w-[330px]"
         src="/riv/1_deposit.riv"
