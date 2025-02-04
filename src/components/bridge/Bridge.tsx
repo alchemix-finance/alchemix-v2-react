e1b1b8f3fbcfd1bdc8b1c80bad6f62d4a2f57452
t0rbik <liker1011@gmail.com>
2025-02-04 21:34:18 +0100

    wormhole bridge on top

--- src/components/bridge/Bridge.tsx
+++ src/components/bridge/Bridge.tsx
@@ -4,8 +4,8 @@ import { WormholeBridge } from "./wormhole/WormholeBridge";
 export const Bridge = () => {
   return (
     <div className="space-y-5">
-      <ConnextBridge />
       <WormholeBridge />
+      <ConnextBridge />
     </div>
   );
 };
