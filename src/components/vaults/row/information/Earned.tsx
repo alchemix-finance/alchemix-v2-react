9925149f2777821020df5ef56a41b538d01f2a0b
t0rbik <liker1011@gmail.com>
2025-02-09 11:52:40 +0100

    Switch earned amount to be queried from subgraph

--- src/components/vaults/row/information/Earned.tsx
+++ src/components/vaults/row/information/Earned.tsx
@@ -1,33 +1,16 @@
-import { useAccount } from "wagmi";
-
 import { Vault } from "@/lib/types";
 import { useVaultEarned } from "@/lib/queries/vaults/useVaultEarned";
-import { useHarvests } from "@/lib/queries/vaults/useHarvests";
 import { formatNumber } from "@/utils/number";
 import { LoadingBar } from "@/components/common/LoadingBar";
-import { CtaButton } from "@/components/common/CtaButton";
 
 export const Earned = ({ vault }: { vault: Vault }) => {
-  const { address } = useAccount();
-
-  const { data: harvestsAndBonuses, isPending: isPendingHarvestsAndBonuses } =
-    useHarvests({ vault });
-
   const {
-    data: generatedEarned,
-    isLoading: isLoadingGeneratedEarned,
-    refetch: generateEarned,
+    data: earned,
+    isPending: isPendingEarned,
     isError,
-  } = useVaultEarned({ vault, harvestsAndBonuses });
+  } = useVaultEarned({ vault });
 
-  const onGenerateEarned = () => {
-    if (!address) return;
-    if (!harvestsAndBonuses) return;
-
-    generateEarned();
-  };
-
-  if (isPendingHarvestsAndBonuses) {
+  if (isPendingEarned) {
     return (
       <div className="flex h-36 items-center justify-center">
         <LoadingBar />
@@ -40,19 +23,7 @@ export const Earned = ({ vault }: { vault: Vault }) => {
       <p>
         {isError
           ? "Error. Please try again"
-          : `${formatNumber(generatedEarned)} ${vault.alchemist.synthType}`}
-      </p>
-      <CtaButton
-        variant="outline"
-        size="sm"
-        onClick={onGenerateEarned}
-        disabled={isLoadingGeneratedEarned || !address}
-      >
-        {isLoadingGeneratedEarned ? "Loading..." : "Generate"}
-      </CtaButton>
-      <p className="text-lightgrey10">
-        Please be patient. It may take up to 2 minutes to calculate the earned
-        amount.
+          : `${formatNumber(earned)} ${vault.alchemist.synthType}`}
       </p>
     </div>
   );
