365c406db006a44ce83bce7291e3f25dcd86a49c
t0rbik <90512605+t0rbik@users.noreply.github.com>
2025-02-17 19:34:45 +0100

    Revert "Merge pull request #427 from alchemix-finance/fix/bridge-rerenders" (#435)
    This reverts commit 6bfeba3b3afa9cead1a67d16c1a6670b05b27a43.

--- src/components/bridge/Bridge.tsx
+++ src/components/bridge/Bridge.tsx
@@ -55,8 +55,6 @@ export const Bridge = () => {
   );
 
   useEffect(() => {
-    if (chain.id === originChainId) return;
-
     if (chain.id === fantom.id) {
       switchChain({
         chainId: bridgeChains[0].id,
@@ -71,20 +69,18 @@ export const Bridge = () => {
       if (newDestinationChainId) {
         setDestinationChainId(newDestinationChainId);
       }
-    } else {
+    } else if (chain.id !== originChainId) {
       setOriginChainId(chain.id);
       const newChainTokenAddress = chainToAvailableTokensMapping[chain.id][0];
       setOriginTokenAddress(newChainTokenAddress);
-      if (chain.id === destinationChainId) {
-        const newDestinationChainId = bridgeChains.find(
-          (c) => c.id !== chain.id,
-        )?.id;
-        if (newDestinationChainId) {
-          setDestinationChainId(newDestinationChainId);
-        }
+      const newDestinationChainId = bridgeChains.find(
+        (c) => c.id !== chain.id,
+      )?.id;
+      if (newDestinationChainId) {
+        setDestinationChainId(newDestinationChainId);
       }
     }
-  }, [chain.id, destinationChainId, originChainId, switchChain]);
+  }, [chain.id, originChainId, switchChain]);
 
   const { data: tokens } = useTokensQuery();
   const [originTokenAddress, setOriginTokenAddress] = useState(() =>
@@ -132,13 +128,25 @@ export const Bridge = () => {
 
   const handleOriginChainSelect = useCallback(
     (chainId: string) => {
-      setAmount("");
       const newChainId = Number(chainId) as SupportedBridgeChainIds;
+
+      setOriginChainId(newChainId);
+      const newChainTokenAddress = chainToAvailableTokensMapping[newChainId][0];
+      setOriginTokenAddress(newChainTokenAddress);
+      setAmount("");
+      if (newChainId === destinationChainId) {
+        const newDestinationChainId = bridgeChains.find(
+          (c) => c.id !== newChainId,
+        )?.id;
+        if (newDestinationChainId) {
+          setDestinationChainId(newDestinationChainId);
+        }
+      }
       switchChain({
         chainId: newChainId,
       });
     },
-    [switchChain],
+    [destinationChainId, switchChain],
   );
 
   const handleDestinationChainSelect = useCallback(
@@ -152,9 +160,13 @@ export const Bridge = () => {
           (c) => c.id !== newChainId,
         )?.id;
         if (newOriginChainId) {
+          setOriginChainId(newOriginChainId);
           switchChain({
             chainId: newOriginChainId,
           });
+          const newChainTokenAddress =
+            chainToAvailableTokensMapping[newOriginChainId][0];
+          setOriginTokenAddress(newChainTokenAddress);
         }
       }
     },
