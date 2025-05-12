b507bacaa42aae50beb5f36ed81596549aed803a
t0rbik <90512605+t0rbik@users.noreply.github.com>
2025-05-12 16:37:08 +0200

    refactor smart account notice (#553)

--- src/components/common/SmartAccountNotice.tsx
+++ src/components/common/SmartAccountNotice.tsx
@@ -1,16 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { zeroAddress } from "viem";
 import { useAccount, usePublicClient } from "wagmi";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { toast } from "sonner";
 
 import { QueryKeys } from "@/lib/queries/queriesSchema";
 import { wagmiConfig } from "@/lib/wagmi/wagmiConfig";
 import { useChain } from "@/hooks/useChain";
 
-const noticeShown: Record<`0x${string}`, boolean> = {};
-
 export const SmartAccountNotice = () => {
+  const toastId = useRef<string | number | null>(null);
+
   const chain = useChain();
   const publicClient = usePublicClient<typeof wagmiConfig>({
     chainId: chain.id,
@@ -27,17 +27,16 @@ export const SmartAccountNotice = () => {
   });
 
   useEffect(() => {
-    if (isSmartAccountUser && !noticeShown[address]) {
-      noticeShown[address] = true;
-      toast(
+    if (isSmartAccountUser && !toastId.current) {
+      toastId.current = toast(
         <div className="space-y-2">
           <h1 className="font-semibold">
             We noticed you are using a smart account.
           </h1>
           <div className="space-y-1">
             <p>
-              Alchemix does not support smart contract interaction unless
-              whitelisted nor smart account interaction.
+              Alchemix does not support Smart Contract or Smart Account
+              interaction unless whitelisted.
             </p>
             <p>
               To get your smart contract whitelisted, reach out in{" "}
@@ -49,8 +48,8 @@ export const SmartAccountNotice = () => {
               >
                 Discord
               </a>
-              . To use Alchemix with an EOA, you must disable smart account
-              functionality.
+              . To use Alchemix with a standard EOA, you must disable smart
+              account functionality.
             </p>
             <p>
               Subscribe to our{" "}
@@ -60,10 +59,10 @@ export const SmartAccountNotice = () => {
                 rel="noopener noreferrer"
                 className="underline"
               >
-                X (Twitter)
+                X (@AlchemixFi)
               </a>{" "}
-              for updates on Alchemix V3, which includes support for smart
-              accounts!
+              for updates on Alchemix v3, which includes support for Smart
+              Accounts!
             </p>
           </div>
         </div>,
@@ -73,7 +72,17 @@ export const SmartAccountNotice = () => {
         },
       );
     }
-  }, [address, isSmartAccountUser]);
+    if (!isSmartAccountUser && toastId.current) {
+      toast.dismiss(toastId.current);
+      toastId.current = null;
+    }
+    return () => {
+      if (toastId.current) {
+        toast.dismiss(toastId.current);
+        toastId.current = null;
+      }
+    };
+  }, [isSmartAccountUser]);
 
   return null;
 };
