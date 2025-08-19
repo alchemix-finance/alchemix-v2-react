3040bc76ff58c953c7608f5b9957f38b1142d02e
t0rbik <90512605+t0rbik@users.noreply.github.com>
2025-08-19 20:48:31 +0200

    update migrator addresses 19.08 (#656)

--- src/lib/config/migrators.ts
+++ src/lib/config/migrators.ts
@@ -3,12 +3,12 @@ import { mainnet, optimism, arbitrum } from "viem/chains";
 export const MIGRATORS = {
   eth: {
     [mainnet.id]: "0x2BEFD8E66dC83523DC25732ea558E878819CaB6F",
-    [optimism.id]: "0x64bf96e1aa1242092430216a12939109015bd780",
-    [arbitrum.id]: "0xb6D30D097773Bf2905aF086B46d9BD269f76910a",
+    [optimism.id]: "0x9687cD7F685Df154475a5235d9d8ded36d0D7c54",
+    [arbitrum.id]: "0x81B1D4318c7950Cf8f1583c74afe3c8FFA1705e8",
   },
   usd: {
     [mainnet.id]: "0x0d4289008e9e566836EFFEA7664eeb60F2ee6fa9",
-    [optimism.id]: "0x0e9e07437bBf496e98cDcDA7A76Fe19294E91eBC",
-    [arbitrum.id]: "0x6e94C527e6a1def124EBf5bE38c8c876a541bFFB",
+    [optimism.id]: "0xB0D682E60a08895146fC07b7042A6424328447a5",
+    [arbitrum.id]: "0xEcEcc1bbd5A239a8502368a4D98bB104A9f146A9",
   },
 } as const;
